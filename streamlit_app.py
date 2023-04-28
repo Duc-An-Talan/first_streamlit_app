8e177af3505c5e0edcd0bffa50441c0b5bf90880
Duc-An-Talan <131973274+Duc-An-Talan@users.noreply.github.com>
2023-04-28 17:39:48 +0200

    Update streamlit_app.py
    rajout du bouton et de la fonction get my fruit load list

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,15 +51,25 @@ streamlit.stop()
 
 
 #import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from fruit_load_list")
 #my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
+
 streamlit.header("the fruit load list contains:")
+#snowflake related function 
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * from fruit_load_list")
+      return my_cur.fetchall()
+# add a button
+if streamlit.button("Get my fruit load list):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_cur = my_cnx.cursor()
+   my_data_rows = get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
+   
+      
 #streamlit.dataframe(my_data_row)
-streamlit.dataframe(my_data_rows)
+
 #rajouter un fruit
 add_my_fruit=streamlit.text_input('What fruit would you like to add?')
 streamlit.write ('Thanks for adding', add_my_fruit)
