493a1ef4408d0d9f905cc1f939031c198e7f4941
Duc-An-Talan <131973274+Duc-An-Talan@users.noreply.github.com>
2023-04-28 16:58:28 +0200

    Update streamlit_app.py
    erreur de parenthese ligne 50

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit=streamlit.text_input('What fruit would you like to add?')
 streamlit.write ('Thanks for adding', add_my_fruit)
 #ça ne va pas marcher correctement
-my_cur.execute("insert into fruit_load_list values ('from streamlit')
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
 
