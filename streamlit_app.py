bf4365f49f673466684e0aee8f395c7acfe4f85e
Duc-An-Talan <131973274+Duc-An-Talan@users.noreply.github.com>
2023-04-28 17:29:09 +0200

    Update streamlit_app.py
    rajout de la fonction get_fruityvice_data(this_choice_fruit)

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,23 +22,28 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page of the selected fruits only 
-
 streamlit.dataframe(fruits_to_show)
 
+
+#create a function 
+def get_fruityvice_data(this_choice_fruit):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_choice_fruit)
+      #streamlit.text(fruityvice_response.json()) # just write the data to the screen
+      # take the json file and normalized it  
+      fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+      return fruityvice_normalized
+   
 # New section to display fruityvice api response
 streamlit.title('Fruityvice Fruit Advice')
 
 try:
    fruit_choice=streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice :
-      streamlit.error("PLease select a fruit in the list to get information")
+      streamlit.error("PLease select a fruit to get information")
    else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-      #streamlit.text(fruityvice_response.json()) # just write the data to the screen
-      # take the json file and normalized it  
-      fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+      back_from_function = get_fruityvice_data(fruit_choice)
       # output a table 
-      streamlit.dataframe(fruityvice_normalized)
+      streamlit.dataframe(back_from_function)
 except URLError as e:
    streamlit.error()
 #pour eviter toute erreur de snowflake
