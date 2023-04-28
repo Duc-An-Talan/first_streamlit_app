6ca4100e6cad0b6d28feca26d0df1ccf231bdd29
Duc-An-Talan <131973274+Duc-An-Talan@users.noreply.github.com>
2023-04-28 16:43:56 +0200

    Update streamlit_app.py
    juste le input en rajout ?

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("the fruit load list contains:")
 #streamlit.dataframe(my_data_row)
 streamlit.dataframe(my_data_rows)
+#rajouter un fruit
+add_my_fruit=streamlit.text_input('What fruit would you like to add?')
 
 
 
